cebf0c838e72c42551d3f43c9980ab479459b361
Christian Wolf <cwolf@3-E-30.42heilbronn.de>
2025-01-28 10:04:46 +0100

    finished

--- client.c
+++ client.c
@@ -6,54 +6,52 @@
 /*   By: cwolf <cwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 09:51:08 by cwolf             #+#    #+#             */
-/*   Updated: 2025/01/27 15:35:17 by cwolf            ###   ########.fr       */
+/*   Updated: 2025/01/28 10:04:04 by cwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	send_signals(int pid, char *str);
+static void	send_signals(int pid, char c);
 
 int	main(int argc, char *argv[])
 {
 	int		pid;
 	char	*str;
+	int		i;
 
+	i = 0;
 	if (argc == 3)
 	{
 		pid = ft_atoi(argv[1]);
 		str = argv[2];
+		while (str[i] != '\0')
+		{
+			send_signals(pid, str[i]);
+			i++;
+		}
 	}
 	else
 	{
 		ft_printf("Sytaxerror! Please insert ./client <PID> <String>\n");
 		exit(1);
 	}
-	send_signals(pid, str);
 }
 
-void	send_signals(int pid, char *str)
+static void	send_signals(int pid, char c)
 {
 	int		i;
-	char	c;
 
-	while (*str)
+	i = 8;
+	while (i > 0)
 	{
-		i = 8;
-		c = *str; // 00101010
-		str++;
-		while (i > 0)
+		i--;
+		if (c >> i & 1)
 		{
-			i--;
-			if (c >> i & 1)
-			{
-				kill(pid, SIGUSR1);
-			}
-			else
-				kill(pid, SIGUSR2);
-			usleep(50); //?
+			kill(pid, SIGUSR1);
 		}
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
 	}
-	//00000000 sschicken
 }
-
--- server.c
+++ server.c
@@ -6,52 +6,35 @@
 /*   By: cwolf <cwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 08:38:01 by cwolf             #+#    #+#             */
-/*   Updated: 2025/01/27 16:57:14 by cwolf            ###   ########.fr       */
+/*   Updated: 2025/01/28 10:03:47 by cwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	action(int signo, siginfo_t *info, void *context)
+static void	action(int signo)
 {
 	static int				i = 0;
-	int						pid;
 	static unsigned char	c = 0;
 
-	(void)context;
-	pid = info->si_pid;
 	c |= (signo == SIGUSR1);
-	if (i < 7)
+	if (++i == 8)
 	{
-		c <<= 1;
-		i++;
-	}
-	else
-	{
-		if (signo == SIGUSR1)
-			c <<= 1; // Letztes Bit verschieben, falls SIGUSR1
-		ft_printf("Test\n");
-		// if (c == '\0')
-		// {
-		// 	//Beenden des Prozesses 
-		// }
 		ft_putchar_fd(c, 1);
 		c = 0;
-		//kill?
+		i = 0;
 	}
+	else
+		c <<= 1;
 }
 
 int	main(void)
 {
-	struct sigaction	s_sigaction;
-
 	ft_printf("My PID: %d\n", getpid());
-	s_sigaction.__sigaction_u.__sa_sigaction = action;
-	s_sigaction.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sigaction, NULL);
-	sigaction(SIGUSR2, &s_sigaction, NULL);
 	while (1)
 	{
+		signal(SIGUSR1, action);
+		signal(SIGUSR2, action);
 		pause();
 	}
 	return (0);
